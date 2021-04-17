f296fa4a41fbeee85730fd42169a09c74ded2ee9
unknown <y.perrot@outlook.fr>
2021-04-17 22:21:55 +0200

    Add basic Redux

--- src/App.js
+++ src/App.js
@@ -1,32 +1,71 @@
 import React from 'react'
+import { connect } from "react-redux"
 
 import KeyBoard from './KeyBoard'
 import DrumManager from './DrumManager'
+import { changeDisplay } from './rootReducer'
 
 import './App.css';
 
-export default class App extends React.Component {
+class App extends React.Component {
 
 	constructor(props) {
 		super(props)
-		this.state = {
-			isOn: true
-		}
-		this.onNewsletterChange = this.onNewsletterChange.bind(this)
+		// this.state = {
+		// 	isOn: true,
+		// 	isMainBank: true,
+		// 	display: ""
+		// }
+		// this.changeDrumState = this.changeDrumState.bind(this)
+		// this.setText = this.setText.bind(this)
+
 	}
 
-	onNewsletterChange() {
-		this.setState({
-			isOn: !this.state.isOn
-		})
-	}
+	// changeDrumState() {
+	// 	this.setState( state => ({
+	// 		isOn: !state.isOn
+	// 	}))
+	// }
+
+	// changeBank() {
+	// 	this.setState( state => ({
+	// 		isMainBank: !state.isMainBank
+	// 	}))
+	// }
+
+	// setText(text) {
+	// 	this.setState({
+	// 		display: text
+	// 	})
+	// }
 
 	render() {
 		return (
-			<div className="App">
-					<KeyBoard />
-					<DrumManager />
+			<div>
+				<h1>{this.props.isOn ? "Ok" : "NOTOK"}</h1>
+				<h1>{this.props.display}</h1>
+				<h1>{this.props.isMainBank ? "Ok" : "NOTOK"}</h1>
+				<button onClick={() => this.props.dispatch(changeDisplay('MyNewMessage'))}>Change Message</button>
 			</div>
+			// <div className="App">
+			// 		<KeyBoard 
+			// 			isOn={this.state.isOn}
+			// 			display={this.state.display}
+			// 			setText={this.state.setText}
+			// 		/>
+			// 		<DrumManager
+			// 			isOn={this.state.isOn}
+			// 			display={this.state.display}
+			// 			changeDrumState={this.state.changeDrumState}
+			// 			setText={this.state.setText}
+			// 		/>
+			// </div>
 		)
 	}
 }
+
+const mapStateToProps = state => {
+	return state
+}
+
+export default connect(mapStateToProps)(App)
---
+++ src/rootReducer.js
@@ -0,0 +1,42 @@
+const CHANGE_MAIN = 'CHANGE_MAIN'
+const CHANGE_DISPLAY = 'CHANGE_DISPLAY'
+const SWITCH = 'SWITCH' 
+
+const initState= {
+    isOn : true,
+    isMainBank: true,
+    display: "DEFAULT",
+}
+
+export function changeMain() {
+    return {
+        type: CHANGE_MAIN
+    }
+}
+
+export function switchDrum() {
+    return {
+        type: SWITCH
+    }
+}
+
+export function changeDisplay(message) {
+    return {
+        type: CHANGE_DISPLAY, 
+        display: message
+    }
+}
+
+export const rootReducer = (state = initState, action) => {
+
+    switch (action.type) {
+        case CHANGE_MAIN:
+            return Object.assign({}, state, {display: !state.isMainBank});
+        case CHANGE_DISPLAY:
+            return Object.assign({}, state, {display: action.display});
+        case SWITCH:
+            return Object.assign({}, state, {isOn: !state.isOn});
+        default:
+            return state
+    }
+}
